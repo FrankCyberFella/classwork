86a515860608db68f7ad2fb8c71fc1565180a684
Frank Fella <[A[A[B[Bfrank.fella@simplilearn.net>
2024-08-20 08:37:54 -0700

    Instructor Push 08/20/2024 08:37:53

--- Unit-2-Fundamental-C#/Day-7-Array-Example/Program.cs
+++ Unit-2-Fundamental-C#/Day-7-Array-Example/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace Day_5_Array_Example
+namespace Day_7_Array_Example
 {
     internal class Program
     {
@@ -12,21 +12,72 @@ namespace Day_5_Array_Example
          *               
          * We will display each or the numbers entered with an indicator if number odd or even,
          *                 their sum and average
+         *
+         * Since we need to do something after we have all the numbers,
+         *     rather than as we get each number
+         *
+         * We need to store the number the user entered and process them all when user is done
+         *
+         * We need to have some way for the user to tell us they are done entering numbers
          *****************************************************************************************/
 
         static void Main(string[] args)
         {
             Console.WriteLine("\nWelcome to my app!");       // Verify the app started
 
-            // Set up a loop to get 5 numbers, one at a time
-            for (int i = 0; i < 5; i++)
-            {
-                // Call the method to get a numeric value
-                double aValue = GetANumber();
+            // Define a constant to use to reference the size of the array
+            const int ARRAY_SIZE = 5;  // use this every where you want to code the size of the array
 
-                // Display the value we got from the method
-                Console.WriteLine("Number entered was: " + aValue);
+            // Define an array to hold up to 10 numbers entered by the user
+            double[] theNumbers = new double[ARRAY_SIZE];  //define an array of 10 doubles
+
+            // Define a variable to hold the user input
+            string whatUserTyped = "";
+
+            // Define a variable to hold the sum of number
+            double theSum = 0;
+
+            // Define a variable to hold the number of variables entered by the user
+            int numberEntered = 0;
+
+            // Set up a loop to get ARRAY_SIZE numbers, one at a time or responses indicating the user is done
+            for (int i = 0; i < ARRAY_SIZE; i++)
+            {
+                if (!moreInput()) // if they don't have any more input...
+                {
+                    break;  // exit the for-loop
+                }
+                // At this point we know the user has a number to enter
+
+                // Call the method to get a numeric value
+                theNumbers[i] = GetANumber();  // get the number and store in the next array element
+
+                numberEntered++;  // Count a number being entered
             }
+            // Since the variable i has the number of times through the loop
+            // Can we use it after the loop to store the number of values entered?
+            //
+            // numberEntered = i;
+            //
+            // NO! i is defined in the for loop - so it can be used inside the for-loop (scope)
+
+            // So now the array has all the numbers entered by the user
+
+            Console.WriteLine("So I received "+ numberEntered + " numbers from you");
+
+            // Go through the array and display each number, whether it's odd or even
+            // Only process the numbers that were entered. i.e. NOT arrayname.length
+            for (int i = 0; i < numberEntered; i++)
+            {
+                Console.WriteLine("Element #: " + i + " is: " + theNumbers[i] 
+                                + " it is " + (theNumbers[i] % 2 == 0 ? "Even" : "Odd"));
+
+                theSum = theSum + theNumbers[i];
+            }
+
+            Console.WriteLine("The sum of the numbers is: " + theSum);
+            Console.WriteLine("The avg of the numbers is: " + theSum / numberEntered);
+
             Console.WriteLine("\nThanks for using my app!"); // Verify the app ended
 
             Console.WriteLine("\nPress enter to end program...");
@@ -36,6 +87,44 @@ namespace Day_5_Array_Example
         /****************************************************************
          * Helper methods used by Main()
          ******************************************************************/
+
+        // return a boolean value to indicate if teh user has more input
+        static bool moreInput()
+        {
+            bool   isThereInput  = false;  // Hold teh return value 
+
+            string whatUserTyped = "";     // Hold what the user enters
+
+            bool   getInput      = true;   // Control the user interaction loop
+
+            do
+            {
+                // Ask the user if they have any numbers to enter (Y/N)
+                Console.WriteLine("Do you have any numbers to enter (Y/N)?");
+                whatUserTyped = Console.ReadLine();
+
+                whatUserTyped = whatUserTyped.ToUpper();
+
+                string firstChar = whatUserTyped.Substring(0, 1);
+
+                if (firstChar == "Y")
+                {
+                    getInput = false;
+                    isThereInput = true;
+                }
+                else
+                {
+                    if (firstChar == "N")
+                    {
+                        getInput = false;
+                        isThereInput = false;
+                    }
+                }
+            } while (getInput); // Loop while we get input
+
+            return isThereInput;
+        }
+
         // method starts with a method signature:  return-type name(parameters)
 
         // This method will get a numeric value from the user

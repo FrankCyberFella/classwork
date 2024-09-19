684a0186b75339242a96de9051a0989a2299a266
Frank Fella <[A[A[B[Bfrank.fella@simplilearn.net>
2024-09-19 15:22:23 -0700

    Instructor Push 09/19/2024 15:22:23

--- Unit-5-Unit-Testing/Day-1-Unit-Testing-V2/CheckingAccountTests/CheckingAcountTests.cs
+++ Unit-5-Unit-Testing/Day-1-Unit-Testing-V2/CheckingAccountTests/CheckingAcountTests.cs
@@ -1,4 +1,7 @@
 using Day_1_Unit_Testing;
+using System.Data.Common;
+using System.Diagnostics.Metrics;
+using System.Transactions;
 using Xunit;
 
 namespace CheckingAccountTests
@@ -46,6 +49,46 @@ namespace CheckingAccountTests
             // Assert - verify we get the expected value
             //          is the balance -100
             Assert.Equal(-100, testAccount.Balance);
+        } // End of a Unit test
+
+         /*    An account has a balance of 0 and attempts a withdrawal of $100
+         *    NOT ALLOWED as the balance after the transaction would be -110 (with overdraft fee) */
+
+        [Fact]
+        public void Withdrawal_Over_Allowed_Overdraft_Amount()
+        {
+            // Arrange - setup test data
+            double startBalance = 0;
+
+            CheckingAccount testAccount = new CheckingAccount("Ryan", startBalance);
+
+            // Act - run the process with test data
+            testAccount.Withdraw(100);
+
+            // Assert - Test results for validity
+            // Since the withdraw should not be allowed, the startBalance should be unchanged
+            Assert.Equal(startBalance, testAccount.Balance);
         }
-    }
-}
+
+        /*    An account has a balance of 100 and attempts a withdrawal of $100
+         *    ALLOWED as the balance after the transaction would be 0           */
+
+        [Fact]
+        public void Withdraw_Of_Allowed_Amount()
+        {
+            // Arrange - Define test data
+
+            double startBalance = 100;
+
+            CheckingAccount testAccount = new CheckingAccount("Jay", startBalance);
+
+            // Act - Run the process with the test data
+
+            testAccount.Withdraw(100);
+
+            // Assert
+
+            Assert.Equal(0, testAccount.Balance);
+        }
+    } // End of the class holding the unit tests
+} // End of namespaces

a1f400efe12de9073ead1391eff1c0db82ecf4ae
FFrank Fella <Frank@CyberAcademy.com>
2025-04-28 18:54:50 -0700

    a

---
+++ Unit-4-Intro-To-Object-Oriented-Programming/Newthing/Newthing/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Newthing;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Method returned: " + AddStarWarsCharacters("bob"));
+
+    } // end of main
+
+    // Methods follows
+    static int AddStarWarsCharacters(string s)
+    {
+        return 0;
+    }
+}

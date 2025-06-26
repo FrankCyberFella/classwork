2c5f994acf78b2506bd76bff26ba61ec758aa211
Frank Fella <Frank@CyberAcademy.com>
2025-06-26 11:41:15 -0700

    Instructor Push 06/26/2025 11:41:14

--- Unit-7-Angular/Day-1-formExample/src/app/components/form-example/form-example.component.ts
+++ Unit-7-Angular/Day-1-formExample/src/app/components/form-example/form-example.component.ts
@@ -11,9 +11,28 @@ import { throwError } from 'rxjs';
 })
 // Allow access to stuff defined this component from outside the component
 export class FormExampleComponent {
+  // Data to be shared with the Angular when processing the web page
   buttonClicked = false
+  firstName = ""     // hold the data from first name from the form
+  lastName = ""      // hold the data from last name from the form
+  birthday = ""      // hold the data from birthday from the form
+  foodPreference = ""
+  emailAddress = ""
+  sendEmail = ""
+  // A checkbox control may have multiple values (many checkboxes can be checked)
+  // Sports is specified as a checkbox it's data will be returned as an array
+  // With each element in the array corresponding to a choice
+  // Checkbox need to be included in the html a individual element
+  // More tomorrow
+  //sports : any[] 
+  sports = ""
+ // Methods to handle interactions with the html/web page
   onSubmit() { // Handle the Submit button click on the  form
     this.buttonClicked = true
     console.log(`OK.... You Clicked Submit`)
+    console.log(`You entered ${this.firstName} in the First Name field`)
+    console.log(`You entered ${this.lastName} in the Last Name field`)
+    console.log(`You entered ${this.emailAddress} in the email field`)
+    console.log(`You entered ${this.sports} as sports you like`)
   }
 }

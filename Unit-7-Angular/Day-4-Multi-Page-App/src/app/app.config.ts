006f0ed7c71e0d78b2508dae313cab85422fad12
Frank Fella <Frank@CyberAcademy.com>
2025-06-30 19:00:37 -0700

    new app

--- Unit-7-Angular/Day-4-Multi-Page-App/src/app/app.config.ts
+++ Unit-7-Angular/Day-4-Multi-Page-App/src/app/app.config.ts
@@ -1,8 +1,19 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter }     from '@angular/router';
 
-import { routes } from './app.routes';
+// Tell the router where to find the array with the routes
+//      that connect a URL to a component
+// the import name must match the export name in app.routes.ts
+import {theRoutes} from './app.routes';  // connect to app.routes.ts (.ts is assumed)
+
+// appConfig object identifies things that you are using that 
+//       are not part of base Angular processing
+// (the router is not required for basic Angular processing)
+//
+// the providers attribute is a list of optional Angular processes you are using
+//  provideRouter indicates you want Angular to router support using the import
+//  specified above
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(theRoutes)]
 };
--- Unit-7-Angular/Day-4-Multi-Page-App/src/main.ts
+++ Unit-7-Angular/Day-4-Multi-Page-App/src/main.ts
@@ -3,6 +3,9 @@
 // This is where you specify the "home" or starting component for the app
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
+// Tells Angular that all stuff for the starting component
+//  is in the /app folder and is prefixed by app.component
+//  in the base folder for the app (./) .src folder
 import { AppComponent } from './app/app.component';
 
 // Start the Angular app with the AppComponent and appConfig

7ef054f05c71e5ea8bec278411c0cfb775ea5cfa
Frank Fella <Frank@CyberAcademy.com>
2025-07-28 14:44:44 -0700

    Instructor Push 07/28/2025 14:44:43

---
+++ Unit-9-Backend-REST-API-Enitty-Framework/GamblerAPI2/GamblerAPI2/src/Controllers/v1/GamblerController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace gamblerapi2.Controllers.v1
+{
+    public class GamblerController : Controller
+    {
+        // GET
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

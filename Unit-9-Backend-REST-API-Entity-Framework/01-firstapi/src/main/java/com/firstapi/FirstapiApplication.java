84b17a180854e2797e33ddb35750a8d573db4135
Frank Fella <Frank@CyberAcademy.com>
2025-07-30 15:26:58 -0700

    Instructor Push 07/30/2025 15:26:57

--- Unit-9-Backend-REST-API-Entity-Framework/01-firstapi/src/main/java/com/firstapi/FirstapiApplication.java
+++ Unit-9-Backend-REST-API-Entity-Framework/01-firstapi/src/main/java/com/firstapi/FirstapiApplication.java
@@ -1,12 +1,24 @@
 package com.firstapi;
 
+// This is generated for you when you create a Spring Boot application.
+//     DO NOT CHANGE ANYTHING IN HERE!!!!!
+
+// Spring Boot is a framework that simplifies the process of building Java applications.
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// This is the main class for the Spring Boot application.
+// It contains the main method which is the entry point of the application.
+// The @SpringBootApplication annotation indicates that this is a Spring Boot application.
+// This is what start when you run the application
+
 @SpringBootApplication
 public class FirstapiApplication {
-
+    // The main method is the entry point of the application.
 	public static void main(String[] args) {
+		// SpringApplication.run() method is used to launch the application.
+		// It takes the application class and command line arguments as parameters.
 		SpringApplication.run(FirstapiApplication.class, args);
 	}
 
--- Unit-9-Backend-REST-API-Entity-Framework/01-firstapi/src/main/java/com/firstapi/myController.java
+++ Unit-9-Backend-REST-API-Entity-Framework/01-firstapi/src/main/java/com/firstapi/myController.java
@@ -1,13 +1,30 @@
 package com.firstapi;
 
+// This file contains controllercode to handle HTTP requests sent to this server
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+// The @RestController annotation indicates that this class has REST controllers in it
+// REST stands for Representational State Transfer,
+//      which is an architectural style for designing networked applications.
+// REST is a type of API - Most popular type of API in the world.
 @RestController
 public class myController {
 
-    @GetMapping
-    public String anyName() {
+    // The @GetMapping annotation maps HTTP GET requests to the specified method.
+    // the following method will handle GET requests to the path http://localhost:8081/
+    //     localhost is name for any process running your computer
+    //     8081 is the port number that the application is running on
+    @GetMapping // This method will handle GET requests to the root URL ("/") or nothing
+    public String anyName() { // the method name can be anything you want
+        // All this control does is return a string that say "Hi There"
         return "Hi There";
+    }
+    // This method will handle GET requests to the path http://localhost:8081/welcome
+    @GetMapping("/welcome")
+    public String controllerMethodNamesDoNotEverMatter() {
+        // This method returns a string that says "Welcome to my first API"
+        return "Welcome to my first API";
     }
 }

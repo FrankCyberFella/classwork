8c233a1104f8d818d39d225da232d6c0fd65b114
Frank Fella <Frank@CyberAcademy.com>
2025-07-31 05:59:05 -0700

    Instructor Push 07/31/2025 05:59:05

--- Unit-9-Backend-REST-API-Entity-Framework/gamblerApi-with-DAO/src/main/java/com/gamblerapi/model/Gambler.java
+++ Unit-9-Backend-REST-API-Entity-Framework/gamblerApi-with-DAO/src/main/java/com/gamblerapi/model/Gambler.java
@@ -1,5 +1,20 @@
 package com.gamblerapi.model;
-import java.util.Objects;
+import  java.util.Objects;
+
+// In the MVC (Model-View-Controller) Server architecture
+// the Model is the data that is being manipulated by the server
+// The Model is a class that represents the data structure
+// In Java we call it a POJO (Plain Old Java Object)
+// In C# it is called a POCO (Plain Old CLR Object)
+//
+// A POJO/POCO is a simple class that has no special requirements
+//
+//  It has: Data members (fields) to hold the data
+//          Constructors to initialize the data members
+//          Getters and Setters to access and modify the data members
+//          toString() method to return a string representation of the object
+//          equals() method to compare two objects for equality
+//          hashCode() method to return a hash code for the object
 
 public class Gambler {
     /***********************************************************************
@@ -14,10 +29,10 @@ public class Gambler {
     private double   Salary;
     private String   BirthDate;
 
-    // 0-arg constructor may be used by C# when needed
+    // 0-arg constructor may be used by Java/C# when needed
     public Gambler() { }
 
-    // 5-arg constructor
+    // 5-arg constructor - accepts all data members
     public Gambler(int id, String name, String address, double salary, String birthDate)
     {
         Id        = id;
@@ -27,46 +42,46 @@ public class Gambler {
         BirthDate = birthDate;
     }
 
-    public int getId() {
+    // Getters and Setters for each data member
+
+    public int    getId() {
         return Id;
     }
-
-    public void setId(int id) {
+    public void   setId(int id) {
         Id = id;
     }
-
     public String getName() {
         return Name;
     }
-
-    public void setName(String name) {
+    public void   setName(String name) {
         Name = name;
     }
-
     public String getAddress() {
         return Address;
     }
-
-    public void setAddress(String address) {
+    public void   setAddress(String address) {
         Address = address;
     }
-
     public double getSalary() {
         return Salary;
     }
-
-    public void setSalary(double salary) {
+    public void   setSalary(double salary) {
         Salary = salary;
     }
-
     public String getBirthDate() {
         return BirthDate;
     }
-
-    public void setBirthDate(String birthDate) {
+    public void   setBirthDate(String birthDate) {
         BirthDate = birthDate;
     }
 
+    // Standard Overrides for methods you want to
+    //    customize from the default behavior
+    //
+    // Most IDEs (like IntelliJ IDEA, Eclipse, etc.)
+    //     will generate these for you
+
+    // toString() method - returns a string representation of the object
     @Override
     public String toString() {
         return "Gambler{" +
@@ -78,6 +93,7 @@ public class Gambler {
                 '}';
     }
 
+    // equals() method - compares two objects for equality
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
@@ -85,6 +101,7 @@ public class Gambler {
         return Id == gambler.Id && Double.compare(Salary, gambler.Salary) == 0 && Objects.equals(Name, gambler.Name) && Objects.equals(Address, gambler.Address) && Objects.equals(BirthDate, gambler.BirthDate);
     }
 
+    // hashCode() method - returns a hash code for the object
     @Override
     public int hashCode() {
         return Objects.hash(Id, Name, Address, Salary, BirthDate);

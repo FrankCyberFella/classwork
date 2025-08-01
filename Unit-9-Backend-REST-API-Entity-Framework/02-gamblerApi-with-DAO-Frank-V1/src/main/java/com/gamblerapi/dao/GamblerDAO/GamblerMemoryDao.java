030034cc405d7149e351e4ac9c9f0902f6b9e79d
Frank Fella <Frank@CyberAcademy.com>
2025-08-01 04:42:11 -0700

    Instructor Push 08/01/2025 04:42:11

--- Unit-9-Backend-REST-API-Entity-Framework/02-gamblerApi-with-DAO-Frank-V1/src/main/java/com/gamblerapi/dao/GamblerDAO/GamblerMemoryDao.java
+++ Unit-9-Backend-REST-API-Entity-Framework/02-gamblerApi-with-DAO-Frank-V1/src/main/java/com/gamblerapi/dao/GamblerDAO/GamblerMemoryDao.java
@@ -10,6 +10,9 @@ public class GamblerMemoryDao {
      *  GamblerDAO Starter Code
      *  
      *  DAO - Data Access Object - Used to Manipulate data in a data source
+     *
+     *  DAOs work with Objects - Receive and return objects
+     *                           unless specific search values
      **************************************************************************/
 
     // Data Source
@@ -28,6 +31,8 @@ public class GamblerMemoryDao {
         gamblers.add(new Gambler(382, "Stickman Nelson", "Cumberland, MD" , 12983.75,   "10/21/1955"));
         gamblers.add(new Gambler(572, "Al Mostbroke"   , "Clayton MO"     , 4505.55,    "01/18/1975"));
     }
+
+    // Methods are called by the controller to get data based in the request it receives
 
     // Method to return all entries in the data source
     public List<Gambler> getGamblers()
@@ -54,19 +59,98 @@ public class GamblerMemoryDao {
         return null;
     } // End of getGamblerById() method
 
-    // Method to retrieve a gambler by name
-    public Gambler getGamblerByName(String name) {
+    // Method to retrieve a gambler whose name contains characters passed
+    // It's possible more than 1 Gambler will meet that criteria
+    // So be sure you return a set of objects (List, Map, Set, Queue, Stack, et al)
+    public List<Gambler> getGamblerByName(String name) {
+        // Define the set to hold teh pbjects to be returned
+        List<Gambler> theGamblers = new ArrayList<Gambler>();
+
         // Loop through the data source to find the gambler with the given name
         for (Gambler aGambler : gamblers) {
             // Check if the current gambler's name matches the given name
             // Use equalsIgnoreCase to compare names without case sensitivity
             // Note use of getter for current gambler's name
-            if (aGambler.getName().equalsIgnoreCase(name)) {
-                return aGambler; // Return the gambler if found
+            if (aGambler.getName().toLowerCase().contains(name.toLowerCase())) {
+                theGamblers.add(aGambler); // Add the Gambler that matches the search criteria to the List
             }
         }
-        // Return null if no gambler with the given name is found
-        return null;
+        // Once we have search teh entire data source and created the set of objects
+        return theGamblers; // Return the set of Gamblers we foudn
     } // End of getGamblerByName() method
+
+    // Add a Gambler to the data source
+    //
+    // 1. Need to have Gambler to add to the data source - a parameter passed to method
+    //         Note: DAOs work with objects NOT individual fields
+    // 2. Add  the Gambler to the data source
+    // 3. If Gambler is added successfully to data source, return object added
+    // 4. If Gambler is NOT added successfully to data source, return null
+    public Gambler addGambler(Gambler aGambler) {   // Receive the Gambler to be added as a parameter
+        // Use Exception Handling to determine if the object was added
+        // If not exception happens it works, if exception happens it didn't
+        try {  // Use exception handling for statements in the black
+            gamblers.add(aGambler);
+        }
+        catch (Exception anExceptionBlock) { // if an Exception happens in the try blocl - do this
+            return null;  // We return null object if the add didn't work
+        }
+        return aGambler;  // If not exception occurred - return the object that was added
+        }
+// if you don't care if it works or not:
+//
+//      gamblers.add(aGambler);
+//      return aGambler;
+//
+// if you want to use the boolean returned by add instead of Exception Handling:
+//
+//     if(gamblers.addGambler(aGambler) { // if the .add returned true....
+//        return aGambler;                //     return the object added
+//     }
+//     else {                    // if the .add didn't return true...
+//           return null;        //      return null;
+//     }
+
+// Update the data source with a Gambler object
+//    (We work in objects NOT individual variables)
+// We expect an object with the updated data already in it
+// (The DAO has not idea how to update individual values in a class
+//          The class is responsible for the data in its object)
+//
+//    1. Need to have Gambler to update in the data source - a parameter passed to method
+//         Note: DAOs work with objects NOT individual fields
+//    2. Update the Gambler in the data source with the data we are passed
+//    3. If Gambler is update successfully to data source, return object update
+//    4. If Gambler is NOT updated successfully to data source, return null
+
+    public Gambler updateGambler(Gambler updateGambler) {
+    //    1. Since our data source (ArrayList) does not have a method to replace and existing element
+    //    2. Remove the existing object
+    //       a. Find the existing object - use a unique value (primary key)
+    //       b. Remove
+    //    3. Add the new object
+
+        // Find the existing Gambler by id by using the existing to do that
+        Gambler existingGambler = getGamblerById(updateGambler.getId());
+
+        // Remove the existing Gambler
+        gamblers.remove(existingGambler);
+
+        // Add the updated gambler
+        gamblers.add(updateGambler);
+
+        return updateGambler;
+    }
+// Delete from the data source a Gambler object
+// Delete is allowed to only receive the primary key of data to be deleted
+// Delete expects the primary key of object to be deleted
+// Delete usually does not return anything
+    public void zapAGambler(int gamblerId) {
+        // 1. Find the object in the ArrayList to be deleted
+        // 2. Remove the object found
+        Gambler existingGambler = getGamblerById(gamblerId);
+        gamblers.remove(existingGambler);
+    }
+
 
 }  // end of GamblerMemoryDao class

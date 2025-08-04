51cd6f2b3045fd0139f22b30065749f1d56c0e39
Frank Fella <Frank@CyberAcademy.com>
2025-08-04 11:25:38 -0700

    Instructor Push 08/04/2025 11:25:38

--- Unit-9-Backend-REST-API-Entity-Framework/02-gamblerApi-with-DAO-Frank-V1/src/main/java/com/gamblerapi/dao/CasinoDAO/CasinoMemoryDao.java
+++ Unit-9-Backend-REST-API-Entity-Framework/02-gamblerApi-with-DAO-Frank-V1/src/main/java/com/gamblerapi/dao/CasinoDAO/CasinoMemoryDao.java
@@ -13,6 +13,12 @@ public class CasinoMemoryDao {
     // Constructor
     public CasinoMemoryDao() {
         this.theCasinos = new ArrayList<Casino>();
+
+        theCasinos.add(new Casino("Caesers Palace"   , "Las Vegas"    , "Julius Caeser"    , 40000));
+        theCasinos.add(new Casino("Harveys Casino"   , "Lake Tahoe"   , "Harvey Wallbanger", 28000));
+        theCasinos.add(new Casino("Ballys"           , "Reno"         , "Bill Bally"       , 31500));
+        theCasinos.add(new Casino("Trump Plaza"      , "Atlantic City", "Donut Trump"      , 22800));
+        theCasinos.add(new Casino("Binions Horseshoe", "Las Vegas"    , "Fred Binyawn"     , 16900));
     }
 
     // Getter

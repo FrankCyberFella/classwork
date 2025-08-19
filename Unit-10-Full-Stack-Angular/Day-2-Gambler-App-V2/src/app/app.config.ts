bfb4841261c2fd02dea84ebcf8af875b6dcd0b0b
Frank Fella <Frank@CyberAcademy.com>
2025-08-18 18:46:50 -0700

    Instructor Push 08/18/2025 18:46:49

--- Unit-10-Full-Stack-Angular/Day-2-Gambler-App-V2/src/app/app.config.ts
+++ Unit-10-Full-Stack-Angular/Day-2-Gambler-App-V2/src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter }      from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
@@ -9,6 +9,6 @@ export const appConfig: ApplicationConfig = {
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient()
+    provideHttpClient() // Tell Angular to provide support HttpClient API calls
   ]
 };
--- Unit-10-Full-Stack-Angular/Day-2-Gambler-App-V2/src/app/app.ts
+++ Unit-10-Full-Stack-Angular/Day-2-Gambler-App-V2/src/app/app.ts
@@ -10,13 +10,20 @@ import { FormsModule }       from '@angular/forms';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
+// Use of ngOnInit() requires implements OnInit on the compnet class
 export class App implements OnInit {
 
   protected title = 'Day-2-Gambler-App-V2';
 
+  // Name the service objectand have it dependency injected into component processing
   constructor(private gamblerApiService : GamblerApiService) {}
 
+  // define a data structure to hold the for the component
+  // (data comes from APU calls)
   theGamblers : any[] = [];
+
+  // If you wil be adding to teh API data source
+  // define an object to hold the data to be added
 
   newGambler : GamblerDataLayout = {
                                       id: 0,
@@ -25,14 +32,20 @@ export class App implements OnInit {
                                       salary : 0,
                                       birthDate : ""
                                    }
-
+// ngOnInit() is run when Angular start processing the page
+// Typically this is where you call the API to get the initial 
+//           copy of the data from the data source
   async ngOnInit() 
   {
   this.theGamblers = await this.gamblerApiService.getAllGamblers();
   }
   
   async addGambler(newGambler : GamblerDataLayout) {
+    // Call the API to add the new object to the data source
     await this.gamblerApiService.addGambler(newGambler)
+    // Refresh the data in the component tp get the new data
+    // (You could also add the new object the data source yourself)
+    //  theGamblers.add(newGambler)
     this.theGamblers = await this.gamblerApiService.getAllGamblers()
   }
   
